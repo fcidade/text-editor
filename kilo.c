04ce04019c100cdc768daefc8a95e433c576b308
Francisco Cidade <franciscocidadehomemjunior@gmail.com>
2023-05-08 23:47:19 -0300

    snap to end of line

--- kilo.c
+++ kilo.c
@@ -242,11 +242,17 @@ void editorMoveCursor(int key) {
         case ARROW_LEFT:
             if (E.cx > 0) {
                 E.cx--;
+            } else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx++;
+            } else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
             }
             break;
         case ARROW_DOWN:
@@ -269,13 +275,15 @@ void editorMoveCursor(int key) {
             E.cx = 0;
             break;
         case END_KEY:
-            if (row) {
-                E.cx = row->size;
-            } else {
-                E.cx = E.screencols - 1;
-            }
+            E.cx = row ? row->size : E.screencols - 1;
             break;
     }   
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
+    }
 }
 
 void editorProcessKeypress(void) {
